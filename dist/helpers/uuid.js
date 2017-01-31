da417d1117d75a47b600e60335de4e5d965c7bdf
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2017-01-31 13:53:22 +1100

    Revert additional changes that moved away from master
    :pear:@christie-metson

--- dist/helpers/uuid.js
+++
@@ -1,7 +0,0 @@
-// Get a universally unique identifier
-'use strict';
-
-var count = 0;
-module.exports = function uuid() {
-  return 'react-tabs-' + count++;
-};
--- dist/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  Tabs: require('./components/Tabs'),
-  TabList: require('./components/TabList'),
-  Tab: require('./components/Tab'),
-  TabPanel: require('./components/TabPanel')
-};
